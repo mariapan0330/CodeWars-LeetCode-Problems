fd05a6563bd12d65a1ee68b97dfbb48a2a188a59
Maria Panagos <maria.pan0330@gmail.com>
2023-05-23 17:36:24 -0500

    rearranged some things, checked edge cases

--- Interview Practice/binary tree/tree_includes.py
+++ Interview Practice/binary tree/tree_includes.py
@@ -28,10 +28,11 @@ def tree_includes(root, target):
         queue.insert(0, curr.right)
     return False
 
-print(tree_includes(node_class.a, 'e'))
-print(tree_includes(node_class.a, 'j'))
-print(tree_includes(node_class.b, 'f'))
-print(tree_includes(node_class.b, 'e'))
+print(tree_includes(None, 'e')) # false
+print(tree_includes(node_class.a, 'e')) # true
+print(tree_includes(node_class.a, 'j')) # false
+print(tree_includes(node_class.b, 'f')) # false
+print(tree_includes(node_class.b, 'e')) # true
 
 def tree_includes_but_recursive(root, target):
     if root == None:
@@ -45,6 +46,7 @@ def tree_includes_but_recursive(root, target):
     return left or right
 
 print("===")
-print(tree_includes_but_recursive(node_class.a, 'e'))
-print(tree_includes_but_recursive(node_class.a, 'j'))
-# print(tree_includes(node_class.b, 'e'))
+print(tree_includes_but_recursive(None, 'e')) # false
+print(tree_includes_but_recursive(node_class.a, 'e')) # true
+print(tree_includes_but_recursive(node_class.a, 'j')) # false
+print(tree_includes_but_recursive(node_class.b, 'e')) # true
